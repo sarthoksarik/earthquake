b4b42329ca49ca5d79a52d8c2665c233a9a2bfd7
sarthoksarik <kirasnamdas@gmail.com>
2019-10-15 22:58:15 +0600

    up

--- eq_explore_data.py
+++ eq_explore_data.py
@@ -7,7 +7,7 @@ with open(eq_raw_data_file) as df:
 readable_eq_file = 'data/readable_eq_data_ss.json'
 
 with open(readable_eq_file, 'w') as rdf:
-    json.dump(readable_eq_data, rdf, indent=4)
+    json.dump(readable_eq_data, rdf, indent=4, separators=('ppp', 'jjj'))
 
 all_eq_data = readable_eq_data['features']
 
